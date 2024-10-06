48f72021c9d29cfa767ac176ccba00394fa6dfb4
Ramitha Heshan <st20286184@outlook.cardiffmet.ac.uk>
2024-10-06 09:34:27 +0530

    client registration

---
+++ login/login/clientRegister.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace login
+{
+    public partial class clientRegister : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("clientLogin.aspx");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
